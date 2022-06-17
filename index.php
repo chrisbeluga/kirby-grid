9133ec96269fe3b31ddde9599916f27ba984a204
Chris Martin <chris@builtbybeluga.com>
2022-06-17 12:35:40 +0100

    move fieldMethods outside of field definition

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
         'fields' => [
             'grid' => [
                 'props' => require_once __DIR__ . '/config/props.php',
-                'fieldMethods' => require_once __DIR__ . '/config/methods.php',
             ]
-        ]
+        ],
+        'fieldMethods' => require_once __DIR__ . '/config/methods.php',
     ]);
